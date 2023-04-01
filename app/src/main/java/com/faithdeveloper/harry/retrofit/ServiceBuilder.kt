92ea893ce6867658d8b3bf42668c861cc5d6ea44
feranmi2002 <oluwaoyehunferanmi@gmail.com>
2023-04-02 00:24:35 +0100

    Removed unused dependencies

--- app/src/main/java/com/faithdeveloper/harry/retrofit/ServiceBuilder.kt
+++ app/src/main/java/com/faithdeveloper/harry/retrofit/ServiceBuilder.kt
@@ -2,7 +2,6 @@ package com.faithdeveloper.harry.retrofit
 
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
